57b618af3b8e78e2db73e53567333d551b88de93
RyanChehab <ryanchehab20@gmail.com>
2026-08-05 15:54:33 +0300

    add CartPage class for shopping cart functionality

---
+++ pages/web/cart.ts
@@ -0,0 +1,21 @@
+import type { Locator } from '@playwright/test';
+import { BasePage } from './base';
+
+/**
+ * Shopping cart view at "/cart"
+ */
+export class CartPage extends BasePage {
+  readonly path = '/cart';
+  readonly orderForm:      Locator = this.page.locator('form#order-handling');
+  readonly checkoutButton: Locator = this.page.locator('#checkoutBtn');
+
+  async open(): Promise<void> {
+    await this.page.goto(this.path);
+    await this.waitReady();
+  }
+
+  async proceedToCheckout(): Promise<void> {
+    await this.checkoutButton.click();
+    await this.waitReady();
+  }
+}
